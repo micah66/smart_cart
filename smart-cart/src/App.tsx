6c04d1d84efb5869453c9bd3b63be13d2fad0d41
micah66 <micahgordon66@gmail.com>
2022-03-30 22:07:10 +0300

    add navigation and basic components for pages

--- smart-cart/src/App.tsx
+++ smart-cart/src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import ShoppingList from './components/ShoppingList';
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import MyCart from './pages/MyCart'
+import CustomLists from './pages/CustomLists'
+import SavedCarts from './pages/SavedCarts'
+import PurchaseHistory from './pages/PurchaseHistory'
+import AccountSettings from './pages/AccountSettings'
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
     <div className="App">
-      <ShoppingList />
+      <div className="logo">
+        <img src="images/logo.jpeg" alt="" />
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />}/>
+            <Route path='my-cart' element={<MyCart />}/>
+            <Route path='custom-lists' element={<CustomLists />}/>
+            <Route path='saved-carts' element={<SavedCarts />}/>
+            <Route path='purchase-history' element={<PurchaseHistory />}/>
+            <Route path='account-settings' element={<AccountSettings />}/>
+            <Route path='*' element={<NoPage />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <footer>Footer content</footer>
     </div>
   );
 }
---
+++ smart-cart/src/pages/SavedCarts.tsx
@@ -0,0 +1,5 @@
+const SavedCarts = () => {
+  return <h1>Saved Carts</h1>
+}
+
+export default SavedCarts
